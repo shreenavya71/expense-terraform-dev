22038d5161039445ee33573d7c874794b7fb69d9
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-07-09 20:32:06 +0100

    first commit

---
+++ 01-vpc/vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+    # source = "../terraform-aws-vpc"
+    source = "git::https://github.com/shreenavya71/terraform-aws-vpc.git?ref=main"
+    project_name = var.project_name     # mandatory parameters
+    common_tags = var.common_tags       # mandatory parameters
+    public_subnet_cidrs = var.public_subnet_cidrs     # mandatory parameters
+    private_subnet_cidrs = var.private_subnet_cidrs  # mandatory parameters  
+    database_subnet_cidrs = var.database_subnet_cidrs  # mandatory parameters
+    is_peering_required = var.is_peering_required
+}
---
+++ 02-sg/main.tf
@@ -0,0 +1,143 @@
+module "db" {
+    source = "../../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for DB MYSQL Instance"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    sg_name = "db"
+}
+
+module "backend" {
+    source = "../../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for Backend Instances" 
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    sg_name = "backend"
+}
+
+module "frontend" {
+    source = "../../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for frontend  Instances"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    sg_name = "frontend"
+}
+
+module "bastion" {
+    source = "../../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for Bastion Instances"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    sg_name = "bastion"
+}
+
+module "ansible" {
+    source = "../../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for Ansible Instances"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+    sg_name = "ansible"
+}
+
+
+# DB is accepting connections from backend--->expense-dev-db on port 3306
+resource "aws_security_group_rule" "db_backend" {
+    type              = "ingress"
+    from_port         = 3306
+    to_port           = 3306
+    protocol          = "tcp"
+    source_security_group_id = module.backend.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.db.sg_id
+}
+
+resource "aws_security_group_rule" "db_bastion" {
+    type              = "ingress"
+    from_port         = 3306
+    to_port           = 3306
+    protocol          = "tcp"
+    source_security_group_id = module.bastion.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.db.sg_id
+}
+
+# Backend is accepting connections from Frontend--->expense-dev-backend on port 8080
+resource "aws_security_group_rule" "backend_frontend" {
+    type              = "ingress"
+    from_port         = 8080
+    to_port           = 8080
+    protocol          = "tcp"
+    source_security_group_id = module.frontend.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.backend.sg_id
+}
+
+resource "aws_security_group_rule" "backend_bastion" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    source_security_group_id = module.bastion.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.backend.sg_id
+}
+
+resource "aws_security_group_rule" "backend_ansible" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    source_security_group_id = module.ansible.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.backend.sg_id
+}
+
+# Frontend is accepting connections from Public--->expense-dev-frontend on port 80
+resource "aws_security_group_rule" "frontend_public" {
+    type              = "ingress"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.frontend.sg_id
+}
+
+resource "aws_security_group_rule" "frontend_bastion" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    source_security_group_id = module.bastion.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.frontend.sg_id 
+}
+
+resource "aws_security_group_rule" "frontend_ansible" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    source_security_group_id = module.ansible.sg_id   # source is where you are getting the traffic from
+    security_group_id = module.frontend.sg_id 
+}
+
+resource "aws_security_group_rule" "bastion_public" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.bastion.sg_id
+}
+
+resource "aws_security_group_rule" "ansible_public" {
+    type              = "ingress"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.ansible.sg_id
+}
