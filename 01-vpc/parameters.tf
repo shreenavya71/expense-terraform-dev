f6a70174344389a2f0cba82a39e764905eea4857
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-07-10 16:28:31 +0100

    second commit

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -20,6 +20,6 @@ resource "aws_ssm_parameter" "private_subnet_ids" {
 
 resource "aws_ssm_parameter" "db_subnet_group_name" {
     name  = "/${var.project_name}/${var.environment}/db_subnet_group_name"
-    type  = "StringList"
+    type  = "String"
     value = module.vpc.database_subnet_group_name  # converting list to string list
 }
