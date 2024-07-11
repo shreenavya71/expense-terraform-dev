f53651ef4eede1c819865aec3524495e1fd91aa8
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-07-11 21:30:45 +0100

    new commit

--- 05-apps/main.tf
+++ 05-apps/main.tf
@@ -55,7 +55,7 @@ module "ansible" {
 }
 module "records" {
     source  = "terraform-aws-modules/route53/aws//modules/records"
-    version = "~> 2.0"
+    version = "~> 3.0"
 
     zone_name = var.zone_name
 
