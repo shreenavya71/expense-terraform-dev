812eb0be3bb4b04b5c15932a5f1924cb188ed533
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-07-11 21:03:20 +0100

    new commit

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dnf install ansible -y
-cd /tmp
+cd /tmp || exit 1
 git clone https://github.com/shreenavya71/expense-ansible-roles.git
-cd expense-ansible-roles
+cd expense-ansible-roles || exit 1
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
